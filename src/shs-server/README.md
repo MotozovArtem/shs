b3d05cc9e9362ee8c06d664ccfa8d781f2324469
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-07 00:30:41 +0300

    [-] Fixed POM files in projects, edited README.md file, add dev profile in maven

--- src/shs-server/README.md
+++ src/shs-server/README.md
@@ -1,7 +1,12 @@
 # SHS (Smart Home System)
 ## Server side
-Language: Go
+Language: Java
 Database: SQLite3
 
 Protocol buffers (shs-api) compile:
 protoc --go_out=paths=source_relative:
+
+## Smart Home System Head Controller
+### How to build:
+In project root:
+`mvn clean package -P dev -DskipTests -Dfile.encoding=UTF-8`
--- src/shs-server/shs-head-controller/src/test/java/ru/rienel/shsheadcontroller/AppTests.java
+++ src/shs-server/shs-head-controller/src/test/java/ru/rienel/shsheadcontroller/AppTests.java
@@ -2,11 +2,16 @@ package ru.rienel.shsheadcontroller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ru.rienel.shsheadcontroller.config.DatabaseProperties;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableConfigurationProperties(DatabaseProperties.class)
 public class AppTests {
 
 	@Test
