c1eeea6d5b88d93cb1583602b07d8d3fd46fcdf7
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-08 00:00:46 +0300

    [-] Add findBy`attribute` method in repositories

--- src/shs-server/shs-head-controller/src/main/java/ru/rienel/shsheadcontroller/repository/PersonRepository.java
+++ src/shs-server/shs-head-controller/src/main/java/ru/rienel/shsheadcontroller/repository/PersonRepository.java
@@ -1,8 +1,15 @@
 package ru.rienel.shsheadcontroller.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ru.rienel.shsheadcontroller.domain.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+	List<Person> findByName(String name);
+
+	List<Person> findBySurname(String surname);
+
+	Person findByNameAndSurnameAndPatronymic(String name, String surname, String patronymic);
 }
--- src/shs-server/shs-head-controller/src/main/java/ru/rienel/shsheadcontroller/repository/ResourceBillRepository.java
+++ src/shs-server/shs-head-controller/src/main/java/ru/rienel/shsheadcontroller/repository/ResourceBillRepository.java
@@ -1,8 +1,11 @@
 package ru.rienel.shsheadcontroller.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ru.rienel.shsheadcontroller.domain.ResourceBill;
 
 public interface ResourceBillRepository extends CrudRepository<ResourceBill, Long> {
+	List<ResourceBill> findBySerialNumber(String serialNumber);
 }
