3d2215116910ff6e16a5856c8d989c94bec03678
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-08 22:39:39 +0300

    [#10] Create domain to DTO converters (and reverse (dto to domain))

---
+++ src/shs-head-controller/src/main/java/ru/rienel/shsheadcontroller/domain/dto/converters/PersonConverter.java
@@ -0,0 +1,32 @@
+package ru.rienel.shsheadcontroller.domain.dto.converters;
+
+import java.util.Objects;
+import javax.validation.constraints.NotNull;
+
+import ru.rienel.shsheadcontroller.domain.Person;
+import ru.rienel.shsheadcontroller.domain.dto.PersonDto;
+
+public class PersonConverter {
+
+	public Person fromDto(@NotNull PersonDto dto) {
+		Objects.requireNonNull(dto);
+
+		Person person = new Person();
+		person.setName(dto.getName());
+		person.setSurname(dto.getSurname());
+		person.setPatronymic(dto.getPatronymic());
+		person.setAddress(dto.getAddress());
+		return person;
+	}
+
+	public PersonDto toDto(@NotNull Person person) {
+		Objects.requireNonNull(person);
+
+		PersonDto dto = new PersonDto();
+		dto.setName(person.getName());
+		dto.setSurname(person.getSurname());
+		dto.setPatronymic(person.getPatronymic());
+		dto.setAddress(person.getAddress());
+		return dto;
+	}
+}
