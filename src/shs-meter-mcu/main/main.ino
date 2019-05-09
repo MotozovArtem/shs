6457d681eca6b7215d0729919d1a66f116b27bbe
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-09 22:08:09 +0300

    [-] Fixed missing libraries

--- src/shs-meter-mcu/main/main.ino
+++ src/shs-meter-mcu/main/main.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
-#include <LiquidCrystal.h>
-#include <MsTimer2.h>
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal lcd(0x3F, 20,4);
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 int kWh = 0;
 float kW = 0;
@@ -11,15 +10,7 @@ int blinksIn10 = 0;
 long duration;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(7, INPUT_PULLUP);
-
-  lcd.init();
-  lcd.clear();
-  lcd.backlight();
-
-  MsTimer::set(10000, MsTimer);
-  MsTimer::start();
 }
 
 void loop() {
