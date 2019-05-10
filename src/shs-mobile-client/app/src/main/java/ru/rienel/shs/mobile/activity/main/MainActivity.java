dc365ddeea6cb6045e94e617b40c20ce9cccc81b
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-11 00:27:25 +0300

    [#20] Fill Main activity: add side menu (without true labels)

--- src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/activity/main/MainActivity.java
+++ src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/activity/main/MainActivity.java
@@ -3,25 +3,13 @@ package ru.rienel.shs.mobile.activity.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import ru.rienel.shs.mobile.R;
-import ru.rienel.shs.mobile.config.AppConfig;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-
-		TextView view = findViewById(R.id.main_activity_text);
-		AppConfig config = new AppConfig(this);
-
-		String endpoint = config.getHeadControllerEndpoint();
-		if (endpoint != null) {
-			view.setText(endpoint);
-		} else {
-			view.setText(R.string.app_name);
-		}
 	}
 }
--- src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/activity/main/MainFragment.java
+++ src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/activity/main/MainFragment.java
@@ -1,8 +1,25 @@
 package ru.rienel.shs.mobile.activity.main;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.rienel.shs.mobile.R;
 
 public class MainFragment extends Fragment implements MainContract.View {
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+		return view;
+	}
+
 	@Override
 	public void setPresenter(MainContract.Presenter presenter) {
 
