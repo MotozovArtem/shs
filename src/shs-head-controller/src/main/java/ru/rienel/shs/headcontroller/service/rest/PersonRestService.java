55759936fb8d6cf96aa3c3b73e4167036d0ba281
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-11 23:34:24 +0300

    [#12] Fill REST controller for Person (not tested) move to On Test

--- src/shs-head-controller/src/main/java/ru/rienel/shs/headcontroller/service/rest/PersonRestService.java
+++ src/shs-head-controller/src/main/java/ru/rienel/shs/headcontroller/service/rest/PersonRestService.java
@@ -2,14 +2,19 @@ package ru.rienel.shs.headcontroller.service.rest;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ru.rienel.shs.headcontroller.repository.PersonRepository;
 import ru.rienel.shs.headcontroller.domain.Person;
+import ru.rienel.shs.headcontroller.repository.PersonRepository;
 
 @RestController
 @RequestMapping("api/v1/person")
@@ -29,5 +34,23 @@ public class PersonRestService {
 			persons.add(person);
 		}
 		return persons;
+	}
+
+	@GetMapping("{id}")
+	public Person getPerson(@PathVariable("id") Long id) {
+		Optional<Person> queryResult = personRepository.findById(id);
+		return queryResult.orElse(null);
+	}
+
+	@PutMapping("/")
+	public Boolean addPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		return savedPerson != null;
+	}
+
+	@DeleteMapping("{id}")
+	public Boolean deletePerson(@PathVariable("id") Long id) {
+		personRepository.deleteById(id);
+		return true;
 	}
 }
