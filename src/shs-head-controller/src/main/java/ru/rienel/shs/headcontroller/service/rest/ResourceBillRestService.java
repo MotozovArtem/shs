6e505c2c9e761d054d6b1b263690281bd93f93b7
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-19 17:16:51 +0300

    [#11] Fill REST controller for ResourceBill: only get

--- src/shs-head-controller/src/main/java/ru/rienel/shs/headcontroller/service/rest/ResourceBillRestService.java
+++ src/shs-head-controller/src/main/java/ru/rienel/shs/headcontroller/service/rest/ResourceBillRestService.java
@@ -1,20 +1,45 @@
 package ru.rienel.shs.headcontroller.service.rest;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.rienel.shs.headcontroller.domain.ResourceBill;
+import ru.rienel.shs.headcontroller.domain.dto.ResourceBillDto;
+import ru.rienel.shs.headcontroller.domain.dto.converters.Converter;
 import ru.rienel.shs.headcontroller.repository.ResourceBillRepository;
 
 @RestController
 @RequestMapping("api/v1/bill")
 public class ResourceBillRestService {
+	@Autowired
 	private ResourceBillRepository resourceBillRepository;
 
 	@Autowired
-	public ResourceBillRestService(ResourceBillRepository resourceBillRepository) {
-		this.resourceBillRepository = resourceBillRepository;
+	private Converter<ResourceBill, ResourceBillDto> converter;
+
+	@GetMapping("/")
+	public List<ResourceBillDto> getAllResourceBills() {
+		List<ResourceBillDto> dtoList = new LinkedList<>();
+		Iterable<ResourceBill> queryResult = resourceBillRepository.findAll();
+		for (ResourceBill resourceBill : queryResult) {
+			dtoList.add(converter.fromDomain(resourceBill));
+		}
+		return dtoList;
 	}
 
-
+	@GetMapping("/{serialNumber}")
+	public List<ResourceBillDto> getAllResourceBillsBySerialNumber(@PathVariable("serialNumber") String serialNumber) {
+		List<ResourceBillDto> dtoList = new LinkedList<>();
+		Iterable<ResourceBill> queryResult = resourceBillRepository.findBySerialNumber(serialNumber);
+		for (ResourceBill resourceBill : queryResult) {
+			dtoList.add(converter.fromDomain(resourceBill));
+		}
+		return dtoList;
+	}
 }
