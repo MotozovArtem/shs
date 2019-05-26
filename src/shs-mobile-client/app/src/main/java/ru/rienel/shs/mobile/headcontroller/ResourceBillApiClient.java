eaf2326dfa77aadb6c34c7cf114143dc1090adb3
Motozov Artyom (Rienel) <mav199824@gmail.com>
2019-05-26 23:14:31 +0300

    [#22] Fill retrofit classes

--- src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/headcontroller/ResourceBillApiClient.java
+++ src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/headcontroller/ResourceBillApiClient.java
@@ -1,4 +1,28 @@
 package ru.rienel.shs.mobile.headcontroller;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+import ru.rienel.shs.mobile.domain.ResourceBill;
+
 public interface ResourceBillApiClient {
+	String BASE_URL = "/api/v1/bill";
+
+	@GET(BASE_URL + "/")
+	Call<List<ResourceBill>> getAllBills();
+
+	@GET(BASE_URL + "/{serialNumber}")
+	Call<ResourceBill> getBillBySerialNumber(@Path("serialNumber") String serialNumber);
+
+	@PUT(BASE_URL + "/")
+	Call<Boolean> addResourceBill(@Body ResourceBill resourceBill);
+
+	@POST(BASE_URL + "/{serialNumber}")
+	Call<Boolean> updateResourceBill(@Body ResourceBill resourceBill);
 }
--- src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/headcontroller/ResourceMeterApiClient.java
+++ src/shs-mobile-client/app/src/main/java/ru/rienel/shs/mobile/headcontroller/ResourceMeterApiClient.java
@@ -1,4 +1,32 @@
 package ru.rienel.shs.mobile.headcontroller;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+import ru.rienel.shs.mobile.domain.ResourceMeter;
+
 public interface ResourceMeterApiClient {
+	String BASE_URL = "/api/v1/meter";
+
+	@GET(BASE_URL + "/")
+	Call<List<ResourceMeter>> getAllResourceMeters();
+
+	@GET(BASE_URL + "/{serialNumber}")
+	Call<ResourceMeter> getResourceMeterBySerialNumber(@Path("serialNumber") String serialNumber);
+
+	@PUT(BASE_URL + "/")
+	Call<Boolean> addResourceMeter(@Body ResourceMeter resourceMeter);
+
+	@POST(BASE_URL + "/{serialNumber}")
+	Call<Boolean> updateResourceMeter(@Body ResourceMeter resourceMeter);
+
+	@DELETE(BASE_URL + "/{serialNumber}")
+	Call<Boolean> deleteResourceMeterBySerialNumber(@Path("serialNumber") String serialNumber);
 }
